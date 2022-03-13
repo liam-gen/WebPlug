e9d2f2fea89166ebf650d45401570c729c4a5473
liamgen <74251382+liam-gen@users.noreply.github.com>
2022-03-13 18:41:16 +0100

    WebPlug v0.0.7

--- examples/website.php
+++ examples/website.php
@@ -1,60 +1,40 @@
 <?php
-include("WebPlug.php");
+require_once("webplug-v0.0.7/src/WebPlug.php");
 
-$d = __DIR__.DIRECTORY_SEPARATOR; // Get the website directory
-
-$p = new WebPlug(array(
+$d = __DIR__.DIRECTORY_SEPARATOR;
+$params = array(
     "settings" => array(
-        "name" => "WebPlug", // Website Name
-        "debug" => true, // Debug initially
-        "scripts" => array("https://cdnjs.cloudflare.com/ajax/libs/bootstrap/4.6.1/js/bootstrap.min.js"), // Scripts included in all pages
-        "icon" => "icon.png", // Icon included on all pages Page (except if the page adds another icon)
-        "stylesheets" => array("style.css") // Styles included in all pages
+        "name" => "WebPlug",
+		"lang" => "en",
+        "scripts" => array("js/script.js"),
+        "icon" => "assets/icon.png",
+        "styles" => array("css/style.css"),
+		"admin" => array(
+			"username" => "admin",
+			"password" => "admin123"
+		)
     ),
     "pages" => array(
-        // 0, 1, 2 are the ids of the pages
-        0  => array(
-            "name" => "{site.name} | Home", // Page Title
-            "link" => "", // Page link (let blank if it's the index)
-            "file" => $d."index.php", // Page file
-            "min" => true, // Minify the code
-            "scripts" => array("https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.js") // Scripts in addition to those included in each page
+        0 => array(
+            "name" => "{site.name} | Home",
+            "link" => "/",
+            "file" => $d."index.php",
+            "scripts" => array("js/jquery.js")
         ),
         1 => array(
-            "name" => "{site.name} | Page", // Page Title
-            "link" => "page", // Page link (let blank if it's the index)
-            "file" => $d."page.php", // Page file
-            "min" => true, // Minify the code
-            "stylesheets" => array("fonts.css"), // Styles in addition to those included in each page
-            "seo" => array( // Page referencing
-                "title" => "WebPlug - Page", // Page Title
-                "description" => "Plugin to create a website simply in php", // Page description
-                "keywords" => "webplug, web, plug, plugin, webplugin, WebPlug, Plugin Web, Web Plugin", // Page keywords
-                "language" => "English", // Page language
-                "author" => "liamgen.js", // Page author
-                "embed" => array( // Embed
-                    "type" => "website", // Embed type
-                    "title" => "WebPlug", // Embed title
-                    "description" => "Plugin to create a website simply in php", // Embed description
-                    "url" => "www.webplugin.tk", // Embed url
-                    "site_name" => "WebPlug", // Embed site name
-                    "image" => "logo.png", // Embed image
-                    ),
-                "twitter" => array( // Twitter embed
-                    "card" => "summary", // Twitter card
-                    "title" => "WebPlug", // Twitter website title
-                    "description" => "Plugin to create a website simply in php", // Twitter website description
-                    "site" => "@webplug", // Twitter website website
-                    "image" => "logo.png", // Twitter website image
-                    "creator" => "liamgen.js" // Twitter website creator
-                )
-            )
+            "name" => "{site.name} | Page",
+            "link" => "page",
+            "file" => $d."page.php",
+            "styles" => array("css/fonts.css"),
         ),
         2 => array(
-            "name" => "{site.name} | Page 2", // Page Title
-            "link" => "page2", // Page url
-            "file" => $d."page2.php" // Page file
-        )
+            "name" => "{site.name} | Notifs",
+            "link" => "notifs",
+            "file" => $d."notifs.php",
+        ),
     ),
-));
-?>
+	"errors" => array(
+		"404" => array("file" => $d."404.php")
+	),
+);
+$p = new WebPlug($params);
